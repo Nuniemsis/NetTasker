0d2fe5058b816dfd1be44e608fb3d1ec2c667aef
Nuno Carvalho dos Santos <nunocarvalhodossantos@gmail.com>
2024-12-18 21:40:17 +0100

    add httpx

--- fastapi-task-app/database/database.py
+++ fastapi-task-app/database/database.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Environment variables
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_USER = os.getenv("DB_USER", "myuser")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "mypassword")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "test_db")
